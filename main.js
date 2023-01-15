a8b7bcae6276a6ddf2450c6f3c4abda2ed33a25e
timbonator <me@timoates.com>
2023-01-15 21:19:23 +0000

    Changed preload method

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-    // Play initial animations on page load.
-        $(window).on('load', function() {
-            $('.hidden').removeClass('hidden');
-        });
+// Play initial animations on page load.
+$(window).on('load', function() {
+    $('.hidden').removeClass('hidden');
+});
+
+// Preload Jet image
+var image = new Image();
+image.src = "jet2.jpg";
